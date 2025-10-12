701121b1c07d7a8cdfaabe30508aaa9413be2cc9
Tarek Alayoubi <tarekalayoubi11@gmail.com>
2025-10-12 22:46:12 +0300

    Initial commit

--- obj/Debug/net8.0/ClinicManagementSystem.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/ClinicManagementSystem.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicManagementSystem.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90cbd9cf305082e9132c4460692d8da5af03b39f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicManagementSystem.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicManagementSystem.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
